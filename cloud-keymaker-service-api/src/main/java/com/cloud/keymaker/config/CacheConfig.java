98840a35dafa9d6f1cab809cc7d35be52e3c4515
S-kwen <61229819+s-kwen@users.noreply.github.com>
2021-03-18 01:26:17 +0800

    支持自定义key redis缓存配置

---
+++ cloud-keymaker-service-api/src/main/java/com/cloud/keymaker/config/CacheConfig.java
@@ -0,0 +1,85 @@
+package com.cloud.keymaker.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @PackageName: com.cloud.keymaker.config
+ * @ClassName: CacheConfig
+ * @Description: This is CacheConfig class by Skwen.
+ * @Author: Skwen
+ * @Date: 2021-03-18 1:10
+ */
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+    @Value("${cache.timeout}")
+    private Long expireTime;
+    @Value("#{${cache.keys}}")
+    private Map<String, Long> keys;
+
+    @Bean(name = "redisTemplate")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(keySerializer());
+        redisTemplate.setHashKeySerializer(keySerializer());
+        redisTemplate.setValueSerializer(valueSerializer());
+        redisTemplate.setHashValueSerializer(valueSerializer());
+        return redisTemplate;
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        //默认缓存配置对象
+        RedisCacheConfiguration redisCacheDefaultConfiguration = RedisCacheConfiguration.defaultCacheConfig();
+        redisCacheDefaultConfiguration = redisCacheDefaultConfiguration.entryTtl(Duration.ofSeconds(expireTime))
+                //设置缓存的默认超时时间
+                .disableCachingNullValues()
+                //如果是空值，不缓存
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer()))
+                //设置key序列化器
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer((valueSerializer())));
+        //设置value序列化器
+
+        if (this.keys != null && !this.keys.isEmpty()) {
+            Map<String, RedisCacheConfiguration> cacheConfigurations = new HashMap<>();
+            for (String key : keys.keySet()) {
+                RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig().serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer()))
+                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer()))
+                        .disableCachingNullValues()
+                        .entryTtl(Duration.ofSeconds(keys.get(key)));
+                cacheConfigurations.put(key, redisCacheConfiguration);
+            }
+            return new RedisCacheManager(RedisCacheWriter.lockingRedisCacheWriter(redisConnectionFactory), redisCacheDefaultConfiguration, cacheConfigurations);
+        } else {
+            return new RedisCacheManager(RedisCacheWriter.lockingRedisCacheWriter(redisConnectionFactory), redisCacheDefaultConfiguration);
+        }
+    }
+
+
+    private RedisSerializer<String> keySerializer() {
+        return new StringRedisSerializer();
+    }
+
+    private RedisSerializer<Object> valueSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
+    }
+}
--- cloud-keymaker-service-api/src/main/java/com/cloud/keymaker/config/RedisConfig.java
+++ cloud-keymaker-service-api/src/main/java/com/cloud/keymaker/config/RedisConfig.java
@@ -25,69 +25,20 @@ import java.util.Map;
  * @Author: Skwen
  * @Date: 2021-03-09 23:23
  */
-@Configuration
-public class RedisConfig extends CachingConfigurerSupport {
-    @Autowired
-    private Properties properties;
-    @Bean
-    public CacheManager cacheManager(RedisConnectionFactory factory) {
-
-
-        RedisCacheConfiguration cacheConfiguration =
-                RedisCacheConfiguration.defaultCacheConfig()
-                        .entryTtl(Duration.ofMinutes(10))
-                        .disableCachingNullValues()
-                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new
-                                GenericJackson2JsonRedisSerializer()));
-        return RedisCacheManager.builder(factory).cacheDefaults(cacheConfiguration).build();
-    }
-
-//    public RedisCacheConfiguration getCacheConfigurationWithTtl(RedisTemplate<String, Object> template, long seconds) {
-//
-//        return RedisCacheConfiguration
-//                .defaultCacheConfig()
-//                // 设置key为String
-//                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(template.getStringSerializer()))
-//                // 设置value 为自动转Json的Object
-//                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(template.getValueSerializer()))
-//                // 不缓存null
-//                .disableCachingNullValues()
-//                // 缓存数据保存1小时
-//                .entryTtl(Duration.ofSeconds(seconds));
-//    }
-//
-//
+//@Configuration
+public class RedisConfig {// extends CachingConfigurerSupport
+//    @Autowired
+//    private Properties properties;
 //    @Bean
-//    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+//    public CacheManager cacheManager(RedisConnectionFactory factory) {
 //
-//        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig()
 //
-//                .entryTtl(Duration.ofMinutes(10)).disableCachingNullValues();//默认过期时间
-//
-//        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
-//
-//        ImmutableSet.Builder<String> cacheNames = ImmutableSet.builder();
-//
-//        ImmutableMap.Builder<String, RedisCacheConfiguration> cacheConfig = ImmutableMap.builder();
-//
-//        for (Map.Entry<String, Duration> entry : properties.getInitCaches().entrySet()) {
-//
-//            cacheNames.add(entry.getKey());
-//
-//            cacheConfig.put(entry.getKey(), defaultCacheConfig.entryTtl(entry.getValue()));
-//
-//        }
-//
-//        return RedisCacheManager.builder(redisCacheWriter)
-//
-//                .cacheDefaults(defaultCacheConfig)
-//
-//                .initialCacheNames(cacheNames.build())
-//
-//                .withInitialCacheConfigurations(cacheConfig.build())
-//
-//                .build();
-//
+//        RedisCacheConfiguration cacheConfiguration =
+//                RedisCacheConfiguration.defaultCacheConfig()
+//                        .entryTtl(Duration.ofMinutes(10))
+//                        .disableCachingNullValues()
+//                        .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new
+//                                GenericJackson2JsonRedisSerializer()));
+//        return RedisCacheManager.builder(factory).cacheDefaults(cacheConfiguration).build();
 //    }
-
 }
